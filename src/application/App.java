75d552d9cd9ecc8ea1bb30b195ffc3fd0eb778f3
Felipe da Costa Catarino <165146810+FelipeCatarino@users.noreply.github.com>
2024-12-05 10:56:19 -0300

    Seller entitie class

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
 
@@ -8,6 +11,9 @@ public class App {
 		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Books", "bob@gmail", new Date(), 3.000, obj);
+		System.out.println(seller);
 
 	}
 
